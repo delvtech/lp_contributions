131ef5e736e2680ba99b1755fc0e517f9405ad3a
Mihai <mcosma@gmail.com>
2023-01-05 13:32:23 -0500

    update comparison files

--- compare_files.py
+++ compare_files.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
-element_deposits = pd.read_csv('./element_depositors.csv')
-element_deposits["datetime"] = pd.to_datetime(element_deposits["evt_block_time"])
-element_deposits = element_deposits.sort_values(by='datetime',ascending=False)
+element_deposits = pd.read_csv(
+    "~/code/retro/jupyter_data/on-chain-analytics/element_depositors.csv"
+)
+# element_deposits["datetime"] = pd.to_datetime(element_deposits["evt_block_time"])
+# element_deposits = element_deposits.sort_values(by='datetime',ascending=False)
 element_deposits.head(10)
